ce230ec28862273e769df1ace7fe5f6c42f24f13
Javier <20200438@ricaldone.edu.sv>
2024-05-09 10:39:41 -0600

    Avance 1

---
+++ app/src/main/java/Fernando/Ortiz/emulador20200438javier/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package Fernando.Ortiz.emulador20200438javier.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
